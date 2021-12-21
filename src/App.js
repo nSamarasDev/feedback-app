a038379ce0e15f6225d291e3b388e998f5475066
“[Nicholas <[n.samaras4@outlook.com]>
2021-12-21 16:13:28 -0500

    update

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react'
+import React from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList'
+import FeedbackData from './components/data/FeedbackData'
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import Card from './components/shared/Card';
+
+
+function App() {
+    const [feedback, setFeedback] = useState(FeedbackData)
+
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback,...feedback])
+    }
+
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you shure you want to delete?')) {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+        
+    }
+
+    return (
+        <>
+        <Header />
+        <div className='container'>
+            <FeedbackForm handleAdd= {addFeedback} />
+            <FeedbackStats feedback={feedback} />
+            <FeedbackList feedback={feedback}
+            handleDelete={deleteFeedback} />
+            <h1></h1>
+        </div>
+        </>
+    )
+}
+
+export default App
---
+++ src/components/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: 'Ad reprehenderit esse et voluptate aliqua pariatur mollit deserunt. Esse quis sit ipsum ullamco amet est occaecat incididunt sunt. Velit ex ex excepteur nisi ea eiusmod dolore do eu est ad voluptate.',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Cillum labore nulla irure est pariatur exercitation quis dolor tempor. Officia culpa voluptate occaecat officia. Pariatur duis aute id labore sit tempor in consequat esse elit. Adipisicing adipisicing aliquip sint mollit exercitation pariatur dolore. Nisi aute nulla officia sint qui excepteur aliquip culpa ipsum pariatur esse ipsum quis. Nisi eiusmod proident nulla amet non ea dolore eu.',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: 'Proident nisi non sint ipsum non veniam magna ut cillum incididunt eu. Deserunt labore sunt dolor duis tempor minim aute quis Lorem aute in occaecat ipsum irure. Commodo magna dolor ut ullamco aute commodo dolore cillum. Reprehenderit sit sit incididunt adipisicing excepteur sint excepteur ut qui. Amet in ex commodo labore nulla aliquip. Et pariatur elit amet irure laboris aliqua pariatur do nostrud excepteur. Amet veniam nostrud nisi nulla consequat do.',
+    },
+];
+
+export default FeedbackData
