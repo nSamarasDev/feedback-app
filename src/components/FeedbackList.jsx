bec2f8aa2fb49c9d99b63e4054ca63e6f91064bc
“[Nicholas <[n.samaras4@outlook.com]>
2021-12-23 16:46:45 -0500

    update

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,37 +1,41 @@
 import {motion, AnimatePresence} from 'framer-motion'
 import {useContext} from 'react'
 import React from 'react'
+import Spinner from './shared/Spinner'
 // import PropTypes from 'prop-types'
 import FeedbackContext from '../context/FeedbackContext'
 
 import FeedbackItem from './FeedbackItem'
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading && (!feedback || feedback.length === 0)) {
         return <p>No feedback yet</p>
     } 
 
-    return (
-        <div className='feedback-list'>
-            <AnimatePresence>
-            {feedback.map((item) => (
-                <motion.div 
-                key={item.id}
-                inital={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-                >
-                    <FeedbackItem 
-                    key={item.id} 
-                    item={item} 
-                    />
-                </motion.div>
-            ))}
-            </AnimatePresence>
-        </div>
-    )
+    return isLoading ? (
+    <Spinner />
+    ) : ( 
+    <div className='feedback-list'>
+    <AnimatePresence>
+    {feedback.map((item) => (
+        <motion.div 
+        key={item.id}
+        inital={{opacity: 0}}
+        animate={{opacity: 1}}
+        exit={{opacity: 0}}
+        >
+            <FeedbackItem 
+            key={item.id} 
+            item={item} 
+            />
+        </motion.div>
+    ))}
+    </AnimatePresence>
+</div>
+)
+    
 }
 
 
--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,31 +1,29 @@
-import {createContext, useState} from 'react';
+import {createContext, useState, useEffect} from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({children}) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'This is feedback item 1',
-            rating: 10,
-        },
-        {
-            id: 2,
-            text: 'This is feedback item 2',
-            rating: 9,
-        },
-        {
-            id: 3,
-            text: 'This is feedback item 3',
-            rating: 7,
-        }
-    ])
+    const [isLoading, setIsLoading] = useState(true)
+    const [feedback, setFeedback] = useState([])
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false
     })
+
+    useEffect(() => {
+        fetchFeedback()
+    }, [])
+
+    //Fetch feedback
+    const fetchFeedback = async () => {
+        const responce = await fetch(`http://localhost:5000/feedback?_sort=id&_order=desc`)
+        const data = await responce.json()
+
+        setFeedback(data)
+        setIsLoading(false)
+    }
 
     //Add Feedback
     const addFeedback = (newFeedback) => {
@@ -59,6 +57,7 @@ export const FeedbackProvider = ({children}) => {
         value={{
             feedback,
             feedbackEdit,
+            isLoading,
             deleteFeedback,
             addFeedback,
             editFeedback,
